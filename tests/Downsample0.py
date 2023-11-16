2de319ee6d3e621633dd114545e5fa859b5bfc8d
Nicholas <102450450+Pakaluk@users.noreply.github.com>
2023-11-15 22:12:58 -0500

    Add files via upload

---
+++ tests/Downsample0.py
@@ -0,0 +1,39 @@
+import time
+import numpy as np
+
+import mindrove
+from mindrove.board_shim import BoardShim, MindRoveInputParams, LogLevels, BoardIds
+from mindrove.data_filter import DataFilter, FilterTypes, AggOperations
+
+
+def main():
+    BoardShim.enable_dev_board_logger()
+
+    # use synthetic board for demo
+    params = MindRoveInputParams()
+    board = BoardShim(BoardIds.SYNTHETIC_BOARD.value, params)
+    board.prepare_session()
+    board.start_stream()
+    BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
+    time.sleep(10)
+    data = board.get_board_data(20)
+    board.stop_stream()
+    board.release_session()
+
+    eeg_channels = BoardShim.get_eeg_channels(BoardIds.SYNTHETIC_BOARD.value)
+    # demo for downsampling, it just aggregates data
+    for count, channel in enumerate(eeg_channels):
+        print('Original data for channel %d:' % channel)
+        print(data[channel])
+        if count == 0:
+            downsampled_data = DataFilter.perform_downsampling(data[channel], 3, AggOperations.MEDIAN.value)
+        elif count == 1:
+            downsampled_data = DataFilter.perform_downsampling(data[channel], 2, AggOperations.MEAN.value)
+        else:
+            downsampled_data = DataFilter.perform_downsampling(data[channel], 2, AggOperations.EACH.value)
+        print('Downsampled data for channel %d:' % channel)
+        print(downsampled_data)
+
+
+if __name__ == "__main__":
+    main()
